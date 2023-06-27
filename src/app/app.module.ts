b408dab05f397c41b0ec7b82d88505e05b306f13
Stanlys <stanlys@yandex.ru>
2023-06-27 18:43:50 +0300

    feat: start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { DxButtonModule } from 'devextreme-angular';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DxButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
